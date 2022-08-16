699b2cd69cfe8080592e12d16d703cadb06a0de3
unknown <luka@maddesign.media>
2022-08-16 09:21:35 +0200

    Implemented report retrieval

--- checker.py
+++ checker.py
@@ -1,20 +1,40 @@
-from bs4 import BeautifulSoup as bs
+from itertools import count
+from math import prod
 import os.path as path
 import urllib.request
 import json
 import requests
+import os
+
+countryNameToId = {}
+countryIdToName = {}
+productNameToId = {}
+productIdToName = {}
 
 def main():
   csv = loadCSV(path.join(path.dirname(__file__), 'input.csv'))
+
+  data = []
+  
+  outputFile = open(path.join(path.dirname(__file__), 'output.csv'), "w")
   for line in csv:
     countryName, productName = line.split(';')
     productId = getProductId(productName)
     countryId = getCountryId(countryName)
 
-    report = getReport(countryId, productId)
-    data = parseReport(report)
+    if countryId == None or productId == None: 
+      print("Skipping {}, {}.".format(countryName, productName))
+      continue
 
-  saveReport(data, path.join(path.dirname(__file__), 'output.csv'))
+    report = getReport(productId, countryId)
+    if report == None:
+      continue
+
+    data = parseReport(report)
+    for line in data:
+      outputFile.write(";".join(line) + "\n")
+      
+  outputFile.close()
 
 
 def loadCSV(path):
@@ -46,17 +66,32 @@ def getProductId(productName, language="en"):
       int: The interneal id of the product.
   """
   id = None
+  productName = productName.replace("\n", "")
+
+  if (productName in productNameToId):
+    print("Found {} in cache.".format(productName))
+    return productNameToId[productName]
+
   url = "https://www.mvorisicochecker.nl/{}/api/products/{}?hideFirstLevel=true".format(language, productName.replace(' ', '%20'))
   content = urllib.request.urlopen(url).read()
   contentJson = json.loads(content)
   products = contentJson["products"]
   if (len(products) > 0) :
     children = products[0]["children"]
-    if (len(children) > 0):
-      firstChild = children[0]
-      id = int(firstChild["id"])
-      print("found id", id)
-      return id
+    if "1" in children:
+      firstChild = children["1"]
+    else:
+      if (len(children) > 0):
+        firstChild = children[0]
+      else:
+        return None
+    id = int(firstChild["id"])
+
+    productNameToId[productName] = int(id)
+    productIdToName[int(id)] = productName
+
+    print("Found product ID {} for {}.".format(id, productName))
+    return id
 
   return id
 
@@ -75,6 +110,12 @@ def getCountryId(countryName, language="en"):
       int: The interneal id of the country.
   """
   id = None
+  countryName = countryName.replace("\n", "")
+  
+  if (countryName in countryNameToId):
+    print("Found {} in cache.".format(countryName))
+    return countryNameToId[countryName]
+
   url = "https://www.mvorisicochecker.nl/{}/api/countries/{}?hideFirstLevel=true".format(language, countryName.replace(' ', '%20'))
 
   content = urllib.request.urlopen(url).read()
@@ -84,7 +125,11 @@ def getCountryId(countryName, language="en"):
   if (len(countries) > 0) :
     firstCountry = countries[0]
     id = int(firstCountry["id"])
-    print("found id", id)
+
+    countryNameToId[countryName] = int(id)
+    countryIdToName[int(id)] = countryName
+
+    print("Found country ID {} for {}.".format(id, countryName))
     return id
 
   return id
@@ -111,9 +156,20 @@ def getReport(productId, countryId, language="en"):
       str: Report returned by api.
   """
   report = None
-  print("Retrieving report for product {} and country {}...".format(productId, countryId))
+  print("Retrieving report for {} ({}) and {} ({})...".format(productIdToName[productId], productId, countryIdToName[countryId], countryId), end="")
   url = "https://www.mvorisicochecker.nl/{}/ajax/generate-report".format(language)
   
+  payload={'product': productId,
+    'country': countryId,
+    'language': language,
+    'report': '33'}
+
+  response = requests.request("POST", url, headers={}, data=payload, files=[])
+
+  if response.status_code == 200:
+    report = json.loads(response.text)
+    print("OK")
+  else: print("NOK")
 
   return report
 
@@ -126,20 +182,17 @@ def parseReport(report):
   Returns:
       dict: Extracted information from report.
   """
-  data = None
-  # TODO: Implement
+  data = []
+  
+  for chapter, chapterContent in report["chapters"].items():
+    for category, categoryContent in chapterContent.items():
+      for riskBody in categoryContent["risks"]:
+        risk = riskBody["body"]
+        risk = risk.replace("<p>", "").replace("</p>", "").replace("\r\n", "").replace("\n", "")
+        data.append([report["country"], report["product"], chapter, category, risk])
+        # print(report["country"], report["product"], chapter, category, risk)
+
   return data
-
-
-def saveReport(data, path):
-  """Save given data as csv to given path.
-
-  Args:
-      data (dict): Data from report.
-      path (str): Output file (csv) path.
-  """
-  # TODO: implement
-  pass
 
 
 if __name__ == '__main__':
